32f974aada703fbd230ab84072434cbcef9e3ec0
The Blackthorn <emekaujah@hotmail.co.uk>
2019-04-16 15:26:54 +1000

    implement parceable

--- app/src/main/java/com/theblackthorn/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/theblackthorn/swoosh/Controller/FinishActivity.kt
@@ -3,9 +3,9 @@ package com.theblackthorn.swoosh.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.theblackthorn.swoosh.Model.Player
 import com.theblackthorn.swoosh.R
-import com.theblackthorn.swoosh.Utilities.EXTRA_LEAGUE
-import com.theblackthorn.swoosh.Utilities.EXTRA_SKILL
+import com.theblackthorn.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
@@ -17,10 +17,9 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
-        searchLeaguesText.text = "Looking for $league $skill league near you..."
+        searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you..."
 
     }
 }
--- app/src/main/java/com/theblackthorn/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/theblackthorn/swoosh/Controller/LeagueActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.theblackthorn.swoosh.Utilities.EXTRA_LEAGUE
+import com.theblackthorn.swoosh.Model.Player
 import com.theblackthorn.swoosh.R
+import com.theblackthorn.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player(league = "", skill = "")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +24,7 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeaguebtn.isChecked = false
 
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked (view: View) {
@@ -31,7 +32,7 @@ class LeagueActivity : BaseActivity() {
         mensLeagueBtn.isChecked = false
         coedLeaguebtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
 
     }
 
@@ -40,14 +41,14 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         mensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View) {
 
-        if (selectedLeague != "") {
+        if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity:: class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         }else{
             Toast.makeText(this, "Please select a League...", Toast.LENGTH_SHORT).show()
